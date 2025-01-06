fbff1d66bc94f4667ac0c7bc5e0f0532793a2f1a
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-01-06 23:37:32 +0100

    Remove unnecessary imports and route the apis

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
-const express = require('express');
-const path = require('node:path');
-const passport = require('passport');
-const session = require('express-session');
-const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
-const prisma = require('./database/prisma');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/indexRouter';
 
 require('dotenv').config();
 
 const app = express();
-cors;
-app.use(express.cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(
-	session({
-		cookie: {
-			maxAge: 7 * 24 * 60 * 60 * 1000, // ms
-		},
-		secret: 'a santa at nasa',
-		resave: true,
-		saveUninitialized: true,
-		store: new PrismaSessionStore(prisma, {
-			checkPeriod: 2 * 60 * 1000, //ms
-			dbRecordIdIsSessionId: true,
-			dbRecordIdFunction: undefined,
-		}),
-	})
-);
-
-require('./config/passport');
-app.use(passport.session());
-
-const indexRouter = require('./routers/indexRouter');
-
-app.use('/', indexRouter);
+app.use('/posts', routes.posts);
+app.use('/posts', routes.comments);
+app.use('/auth', routes.auth);
 
 app.listen(3000, console.log('express server is listening on port 3000'));
