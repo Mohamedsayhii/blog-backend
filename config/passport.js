8c95330ad17fe4ae1dbd10c3e35c75ab1a7ae332
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-01-07 23:51:52 +0100

    Implement JWT

--- config/passport.js
+++ config/passport.js
@@ -1,16 +1,28 @@
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const prisma = require('../database/prisma');
+import bcrypt from 'bcryptjs';
+import passport from 'passport';
+import { ExtractJwt, JwtStrategy } from 'passport-jwt';
+import prisma from '../database/prisma';
+require('dotenv').config();
 
-passport.use();
+const opts = {
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	secretOrKey: process.env.SECRET,
+};
 
-passport.serializeUser((user, done) => done(null, user.id));
+passport.use(
+	new JwtStrategy(opts, async (jwt_payload, done) => {
+		try {
+			const user = prisma.user.findUnique({
+				where: { email: jwt_payload.email },
+			});
 
-passport.deserializeUser(async (id, done) => {
-	try {
-		const user = await prisma.user.findUnique({ where: { id } });
-		done(null, user);
-	} catch (err) {
-		done(err);
-	}
-});
+			if (!user) {
+				return done(null, false);
+			}
+
+			return done(null, jwt_payload);
+		} catch (error) {
+			return done(error, false);
+		}
+	})
+);
