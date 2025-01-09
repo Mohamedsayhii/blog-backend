96000c2f28e937057262d4b911561f716ade7b91
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-01-09 23:48:08 +0100

    Add login and sign functionalities

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,4 +2,7 @@ import { Router } from 'express';
 
 const authRouter = Router();
 
+authRouter.post('/', authController.login);
+authRouter.post('/', authController.signup);
+
 export default authRouter;
