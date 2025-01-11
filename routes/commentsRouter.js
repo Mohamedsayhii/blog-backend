6d4d68a85db2359509316a8fd77a73794b7eb3dc
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-01-11 16:28:02 +0100

    Add comments router endpoints

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -1,5 +1,17 @@
 import { Router } from 'express';
+import commentsController from '../controllers/commentsController';
 
 const commentsRouter = Router();
+
+commentsRouter.get('/:postId/comments', commentsController.getAllComments);
+commentsRouter.post('/:postId/comments', commentsController.createComment);
+commentsRouter.put(
+	'/:postId/comments/:commentId',
+	commentsController.updateComment
+);
+commentsRouter.delete(
+	'/:postId/comments/:commentId',
+	commentsController.deleteComment
+);
 
 export default commentsRouter;
