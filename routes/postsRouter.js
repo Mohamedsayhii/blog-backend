674960549a6a5d37a0cc587fc228f497f41a3722
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-01-11 16:19:08 +0100

    Add posts router endpoints

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import postController from '../controllers/postController';
 
 const postsRouter = Router();
+
+postsRouter.get('/', postController.getAllPosts);
+postsRouter.post('/', postController.createPost);
+postsRouter.get('/:postId', postController.getPost);
+postsRouter.put('/:postId', postController.updatePost);
+postsRouter.delete('/:postId', postController.deletePost);
 
 export default postsRouter;
