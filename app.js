e693c2acc7f3109f224bfd283751299b82651b8c
Mohamedsayhii <mohamedsayhii@outlook.com>
2025-03-23 16:13:56 +0100

    Add controller for errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/indexRouter.js';
+import errorHandler from './controllers/errorController.js';
 
 const app = express();
 app.use(cors());
@@ -11,5 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/posts', routes.posts);
 app.use('/posts', routes.comments);
 app.use('/auth', routes.auth);
+
+app.use(errorHandler);
 
 app.listen(3000, console.log('express server is listening on port 3000'));
